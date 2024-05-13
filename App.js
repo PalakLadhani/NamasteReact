1815dbc5c56ae3608176215f92a59dbb5eb2c1c3
PalakLadhani <pladhani3@gmail.com>
2024-05-13 23:03:44 +0530

    todays update

--- App.js
+++ App.js
@@ -1,37 +1,210 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement=>ReactElement-JS object=>HTMLElement(render)
+/**
+ * Header
+ *  -Logo
+ *  -Nav Items
+ * Body
+ *  -Search
+ *  -RestaurantContainer
+ *    -RestaurantCard
+ *      -Img
+ *      -Name of Restaurent,Star Rating,cuisine,deleivery time
+ * Footer
+ *    - copyright
+ *    -Links
+ *     -Address
+ *     -Contact
+ *
+ */
 
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste React🚀");
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// console.log(heading);
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "lavender",
+      }}
+    >
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          resData.data.cloudinaryImageId
+        }
+      />
+      <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisines.join(", ")}</h4>
+      <h4>{resData.data.avgRating}</h4>
+      <h4>{resData.data.costForTwo / 100}</h4>
+      <h4>{resData.data.deliveryTime}minutes</h4>
+    </div>
+  );
+};
 
-//JSX-is not HTML in JS
-// JSX-HTML-LIKE or XML-LIKE  SYNTAX
-// JSX (transpiled before it reaches the JS)-PARCEL-Babel
-// JSX=> Babel transpiles to Reac t.createElement=>ReactElement-JS object=>HTMLElement(render)
+const resObj = {
+  type: "restaurant",
+  data: {
+    type: "F",
+    id: "74453",
+    name: "Domino's Pizza",
+    uuid: "87727dbd-7f2b-4857-9763-359624165845",
+    city: "21",
+    area: "Athwa",
+    totalRatingsString: "1000+ ratings",
+    cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+    cuisines: ["Pizzas"],
+    tags: [],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    deliveryTime: 45,
+    minDeliveryTime: 45,
+    maxDeliveryTime: 45,
+    slaString: "45 MINS",
+    lastMileTravel: 0,
+    slugs: {
+      restaurant: "dominos-pizza-majura-nondh-test_surat",
+      city: "surat",
+    },
+    cityState: "21",
+    address:
+      "Shop 32 To 35, Sheetal Shopping Square,Near Lb Turning Point, Bhatar Road,MAJURA NONDH, Surat,GUJARAT-395001",
+    locality: "Bhatar Road",
+    parentId: 2456,
+    unserviceable: false,
+    veg: true,
+    select: false,
+    favorite: false,
+    tradeCampaignHeaders: [],
+    aggregatedDiscountInfo: {
+      header: "FREE DELIVERY",
+      shortDescriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      subHeader: "",
+      headerType: 0,
+      superFreedel: "",
+    },
+    aggregatedDiscountInfoV2: {
+      header: "",
+      shortDescriptionList: [
+        {
+          meta: "Free Delivery",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      subHeader: "",
+      headerType: 0,
+      superFreedel: "",
+    },
+    chain: [],
+    feeDetails: {
+      fees: [],
+      totalFees: 0,
+      message: "",
+      title: "",
+      amount: "",
+      icon: "",
+    },
+    availability: {
+      opened: true,
+      nextOpenMessage: "",
+      nextCloseMessage: "",
+    },
+    longDistanceEnabled: 0,
+    rainMode: "NONE",
+    thirdPartyAddress: false,
+    thirdPartyVendor: "",
+    adTrackingID: "",
+    badges: {
+      imageBased: [],
+      textBased: [],
+      textExtendedBadges: [],
+    },
+    lastMileTravelString: "2.1 kms",
+    hasSurge: false,
+    sla: {
+      restaurantId: "74453",
+      deliveryTime: 45,
+      minDeliveryTime: 45,
+      maxDeliveryTime: 45,
+      lastMileTravel: 0,
+      lastMileDistance: 0,
+      serviceability: "SERVICEABLE",
+      rainMode: "NONE",
+      longDistance: "NOT_LONG_DISTANCE",
+      preferentialService: false,
+      iconType: "EMPTY",
+    },
+    promoted: false,
+    avgRating: "4.0",
+    totalRatings: 1000,
+    new: false,
+  },
+  subtype: "basic",
+};
 
-// React Element
-// const jsxHeading =(<h1 id="heading">Namaste React using JSX🚀</h1>) ;
-const heading = <h1 id="heading">Namaste React using JSX🚀</h1>;
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        <RestaurantCard resData={resObj} />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-// React Functional Components
-// Component Composition
-const Title = () => <h1>Namaste React using JSX🚀</h1>;
-
-// Same above and below syntax
-
-const HeadingComponent = () => (
-  <div id="conatiner">
-    {/* <Title />
-    <Title></Title> */}
-    {Title()}
-    {/* we can write it so many times that above thing and above all are same things  */}
-    <h1>Namaste React Functional Component</h1>
-  </div>
-);
-
-// <HeadingComponent
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
