97f00e0830dafe59f72ed4e38b4320bd9640275e
PalakLadhani <pladhani3@gmail.com>
2024-05-20 23:34:15 +0530

    todays update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-const Header = () => {
-  return (
-    <div className="Header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
-          alt="logo"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
 const RestaurantCard = ({ resData }) => {
   return (
     <div className="res-card" style={{ backgroundColor: "lavender" }}>
