441a268af74a15671bf42347f7e7ed2fbdb0c586
PalakLadhani <pladhani3@gmail.com>
2024-05-22 00:49:02 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-const RestaurantCard = ({ resData }) => {
-  return (
-    <div className="res-card" style={{ backgroundColor: "lavender" }}>
-      <img
-        className="res-logo"
-        alt="res-logo"
-        src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660" +
-          resData.data.cloudinaryImageId
-        }
-      />
-      <h3>{resData.data.name}</h3>
-      <h4>{resData.data.cuisines.join(", ")}</h4>
-      <h4>{resData.data.avgRating}</h4>
-      <h4>{resData.data.costForTwo / 100}</h4>
-      <h4>{resData.data.deliveryTime} minutes</h4>
-    </div>
-  );
-};
-
 const Body = () => {
   const [restaurants, setRestaurants] = useState(null);
 
---
+++ src/Components/RestaurantCard.js
@@ -0,0 +1,19 @@
+const RestaurantCard = ({ resData }) => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "lavender" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660" +
+          resData.data.cloudinaryImageId
+        }
+      />
+      <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisines.join(", ")}</h4>
+      <h4>{resData.data.avgRating}</h4>
+      <h4>{resData.data.costForTwo / 100}</h4>
+      <h4>{resData.data.deliveryTime} minutes</h4>
+    </div>
+  );
+};
